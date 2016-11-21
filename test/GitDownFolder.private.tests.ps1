f4ed952458ef79b7a9fb3c9d31ce81997d957a81
Roberto Desideri <robertodesideri@outlook.com>
2016-11-21 16:15:16 +0100

    add dependencies first tests

--- test/GitDownFolder.private.tests.ps1
+++ test/GitDownFolder.private.tests.ps1
@@ -15,12 +15,34 @@ Foreach($import in @($Private))
     }
 }
 
+Function New-CustomObject
+{
+	$a = New-Object -TypeName PSCustomObject
+	Return $a
+}
+
+Function New-CustomTypedObject ($TypeName)
+{
+	$a = New-Object -TypeName $TypeName
+	Return $a
+}
+
 #Test begin!
 
 Describe "GitDownFolder Private Functions" {
-	Context "Function Exists" {
-		It "Should Return" {
-		
+	Context "Function return expected value / type" {
+		$co = New-CustomObject
+		It "Should Return specific object" {
+			Mock -CommandName Get-Singleton -MockWith {Return $co }
+			Get-GlobalGitDataObject | Should Be $co
+		}
+		It "Should return a System.Uri type object"{
+			$url = "https://google.com"
+			Get-SystemUriFromUrl -Url $url | Should BeOfType System.Uri
+		}
+		It "" {
+			[System.Uri]::new("")
+			New-GlobalGitDataObject -UrlToParse
 		}
 	}
 }
